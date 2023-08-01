5c6751a5487c89f8a5f352c57821a7e28ff64c19
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-01 07:26:12 -0700

    Added firebase config, updated server.js to include custom middleware for authentication

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
+require('dotenv').config();
+const { initializeApp } = require('firebase-admin/app');
+module.exports.firebase = initializeApp();
 const express = require('express');
-const PORT = 3000 || process.env.PORT;
-
 const app = express();
+const morgan = require('morgan');
+const router = require('./routes.js');
+const { decodeToken } = require('./middleware');
+
+
 app.use(morgan('dev'))
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
 
-var router = require('./routes.js');
-app.use (router);
+app.use(decodeToken);
 
+app.get('/test', (req, res) => {
+  res.send('hello users world');
+});
+
+app.use('/api', router);
+
+app.all('/', (req, res) => {
+  res.status(404).end();
+});
+
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
